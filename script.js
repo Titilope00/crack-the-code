57bccf92a66a7c8c282ab97b6f553f9464bebf5b
Titilop00 <oluwaropoblessing03@gmail.com>
2024-03-22 06:29:05 -0700

    second commit

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-const randomNumber = Math.floor(Math.random() * 100) + 1;
-console.log(randomNumber);
+let randomNumber = generateRandomNumber();
+let score = 0;
+
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+}
 
 function checkGuess() {
     const userGuess = parseInt(document.getElementById('guessInput').value);
-    
+
     if (userGuess === randomNumber) {
-        displayFeedback('Congratulations! You guessed the correct number!🥰🎊');
+        displayFeedback('Congratulations! You guessed the correct number!🥰');
+        score += 5; // Increase score by 5 diamond points
+        document.getElementById('score').innerText = `Score: ${score}`;
+        randomNumber = generateRandomNumber();
+        setTimeout(clearInput, 8000); // Clear input field after 8 seconds
     } else if (userGuess < randomNumber) {
-        displayFeedback('Too low. Try again!😎');
+        displayFeedback('Too low. Try again!😥');
     } else {
-        displayFeedback('Too high. Try again!😥');
+        displayFeedback('Too high. Try again!🙂');
     }
 }
 
 function displayFeedback(message) {
-    const feedback = document.getElementById('feedback');
-    feedback.innerHTML = message;
-    feedback.style.animation = 'fadeIn 1s ease-in-out';
+    document.getElementById('feedback').innerText = message;
+}
+
+function clearInput() {
+    document.getElementById('guessInput').value = '';
 }
